1c11b452aea2ef679f71c8e0241cdec07e82a533
Arga <argaprataman052@gmail.com>
2024-03-08 01:04:10 +0700

    responsive homepage

--- src/components/navbar/navbarhome.js
+++ src/components/navbar/navbarhome.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="flex items-center justify-between bg-transparent rounded-full mt-5 mx-10 px-6 py-3 h-20 z-50">
       <div className="logo">
         <Image src="/logo.png" alt="logo" height={100} width={130} />
       </div>
-      <ul className="flex text-white text-lg bg-black bg-opacity-20 w-[30rem] h-[4rem] justify-center rounded-full items-center backdrop-blur-lg">
+
+      {/* Hamburger Menu */}
+      <div className="block md:hidden">
+        <button onClick={toggleNavbar} className="text-white focus:outline-none">
+          <svg className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            {isOpen ? <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /> : <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />}
+          </svg>
+        </button>
+      </div>
+
+      {/* Navbar Items */}
+      <ul className={`md:flex md:items-center md:justify-center md:bg-black md:bg-opacity-20 md:w-[30rem] md:h-[4rem] md:rounded-full ${isOpen ? "block" : "hidden"} md:block`}>
         <li>
           <Link href="/">Beranda</Link>
         </li>
@@ -27,8 +45,14 @@ const Navbar = () => {
         <li>
           <Link href="/jadwal">Jadwal</Link>
         </li>
+        <li className="mx-2">|</li>
+        <li>
+          <Link href="/forum">Forum</Link>
+        </li>
       </ul>
-      <div className="btn-forum flex">
+
+      {/* Forum Button */}
+      <div className="btn-forum hidden md:flex">
         <button className="bg-white text-black font-bold py-2 px-4 rounded-full">
           <Link href="/forum">Forum</Link>
         </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,51 +16,54 @@ const HomePage = () => {
   };
 
   return (
-    <main className="h-screen grid place-items-center">
-      <div className="bg-blue-500 h-[47rem] w-[95rem] rounded-lg mt-[2rem]">
-      <Navbar />
+    <main className="lg:m-10 m-2">
+      <div className="bg-blue-500 h-screen rounded-lg">
+        <Navbar />
       </div>
       {/* start smartchat */}
-      <div className="bg-[#2F2D2D] h-[50rem] w-[90rem] mt-[-5rem] rounded-lg">
-        <p className="text-white m-4 text-2xl font-semibold">SmartChat</p>
-        <div className="w-[90rem] h-[40rem] flex flex-col justify-center items-center">
-          <Image src="/smartchat-logo.png" alt="logo" height={100} width={100} />
-          <p className="text-center mt-2 text-[#787070]">
+      <div className="bg-[#2F2D2D] relative h-screen md:mx-12 mx-2 mt-[-9rem] rounded-lg flex justify-center items-center flex-col">
+        <p className="text-white absolute left-5 top-2 text-2xl font-semibold">SmartChat</p>
+        <div className="w-full h-[40rem] flex flex-col justify-center items-center">
+          <Image src="/smartchat-logo.png" alt="logo" height={100} width={100} className="mx-auto w-[15%] md:w-[10%] lg:w-[7%] object-contain" />
+          <p className="text-center mt-2 text-[#787070] md:text-base lg:text-sm text-[12px]">
             Berikan pertanyaan anda seputar eskul yang ada di <br /> SMK NEGERI 1 SUBANG
           </p>
         </div>
-        <div className="flex justify-center">
-          <div className="relative">
-            <input type="text" placeholder="Cari jawaban" className="w-[80rem] px-4 py-4 rounded-lg focus:outline-none" />
-            <button className="absolute inset-y-2 right-2 flex items-center px- py-4 border border-[#828282] rounded-lg">
-              <Image src="/kirim.png" alt="icon logo" height={50} width={50} />
-            </button>
-          </div>
+
+        <div className="relative w-full md:px-10 px-5">
+          <input type="text" placeholder="Cari jawaban" className="w-full px-4 py-4 rounded-lg focus:outline-none" />
+          <button className="absolute inset-y-2 md:right-12 right-7 flex items-center border border-[#828282] rounded-lg">
+            <Image src="/kirim.png" alt="icon logo" height={50} width={50} />
+          </button>
         </div>
       </div>
       {/* end smartchat */}
       {/* start about */}
-      <div className="bg-[#2F2D2D] h-[50rem] w-[95rem] rounded-lg mt-[2rem]">
-        <div className="grid grid-cols-3 gap-4 m-10">
-          <div className="col-span-2">
-            <p className="text-4xl text-white w-[50rem]">
-              Terdapat beragam ekskul yang mencakup 36 pilihan kegiatan ekstrakurikuler. Mulai dari bidang olahraga, seni, hingga teknologi, para siswa memiliki peluang untuk mengembangkan bakat dan minat mereka melalui berbagai kegiatan
-              yang menarik dan mendidik.
-            </p>
+      <div className="bg-[#2f2d2d] h-auto rounded-lg mt-[2rem] relative">
+        <div className="col-span-3 mx-5 lg:w-[50rem] w-[19rem]">
+          <p className="pt-5 text-xl md:text-4xl font-bold text-[#fff]">Tentang Eskul</p>
+          <p className="pt-2 text-white  md:text-xl lg:text-2xl">
+            Terdapat beragam ekskul yang mencakup 36 pilihan kegiatan ekstrakurikuler. Mulai dari bidang olahraga, seni, hingga teknologi, para siswa memiliki peluang untuk mengembangkan bakat dan minat mereka melalui berbagai kegiatan yang
+            menarik dan mendidik.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mx-5 lg:mx-10 mt-5 lg:mt7">
+          <div className="flex items-center justify-start">
+            <Image src="/logo.png" alt="logo" height={30} width={100} />
           </div>
-          <div className="ps-[15rem]">
-            <ul className="text-white text-3xl">
-              <li>
+          <div className="flex justify-center items-center flex-col">
+            <ul className="text-white text-xl flex">
+              <li className="m-2">
                 <Link href="/" onClick={(e) => handleLinkClick("/1.JPG", "/2.JPG", "/galeri", e)}>
                   Galeri
                 </Link>
               </li>
-              <li className="pt-2">
+              <li className="m-2">
                 <Link href="/" onClick={(e) => handleLinkClick("/2.JPG", "/1.JPG", "/pengumuman", e)}>
                   Pengumuman
                 </Link>
               </li>
-              <li className="pt-2">
+              <li className="m-2">
                 <Link href="/" onClick={(e) => handleLinkClick("/profil.png", "/profil-image.jpg", "/profil", e)}>
                   Profil
                 </Link>
@@ -68,61 +71,48 @@ const HomePage = () => {
             </ul>
           </div>
         </div>
-        <div className="grid grid-cols-3 gap-2 m-10">
-          <Image src="/logo.png" alt="logo about" width={300} height={300} className="mt-[17rem]" />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 mx-5 lg:mx-10">
+          <Image src="/logo.png" alt="logo about" width={300} height={300} className="mt-" />
           <Image src={image1} alt="eskul marching band" width={500} height={500} className="h-[22rem] rounded-lg mt-[1rem]" />
           <Image src={image2} alt="eskul marching band" width={500} height={500} className="h-[22rem] rounded-lg mt-[1rem]" />
         </div>
-        <div className="flex justify-center mt-[-1.5rem] ms-[-20rem]">
-          <button className="border border-[#fff] text-white font-bold py-[-1rem] px-4 rounded-full">
-            <Link href={galleryLink}>Lihat semua</Link>
+
+        <div className="flex justify-end lg:me-10 me-5 mt-5">
+          <button className="border border-[#fff] text-white font-semibold px-2 rounded-full">
+            <Link href={galleryLink} className="text-sm lg:text-lg">
+              Lihat semua
+            </Link>
           </button>
-          <button className="bg-white py-4 px-4 rounded-full ">
-            <Link href={galleryLink}>
-              <Image src="/next-to-forum.png" alt="next-to-forum" height={25} width={25} className="rotate-[-25deg]" />
+          <button className="bg-white h-6 w-6 lg:h-10 lg:w-10 rounded-full">
+            <Link href={galleryLink} className="flex justify-center items-center flex-col">
+              <Image src="/next-to-forum.png" alt="next-to-forum" height={25} width={25} className="rotate-[-25deg] w-[60%] md:w-[70%] lg:w-[70%] object-contain" />
             </Link>
           </button>
         </div>
+        <div className="h-10"></div>
       </div>
       {/* end about */}
       {/* start footer */}
-      <div className="bg-[#2F2D2D] h-[13rem] w-[95rem] rounded-lg mt-[2rem] px-10 mb-5" style={{ marginBottom: "2rem" }}>
-        <p className="text-[#fff] text-lg mt-5">Hubungi kami</p>
-        <div className="flex justify-between mt-5">
-          <p className="text-white text-3xl">
-            Terima kasih telah mengunjungi situs kami. <br /> Kami senang bisa berbagi dengan Anda.
-          </p>
-          <div className="flex mt-10">
-            <Image src="/facebook.png" alt="icon sosmed facebook" height={30} width={30} style={{ marginRight: "10px", width: "auto", height: "30px " }} />
-            <Image src="/youtube.png" alt="icon sosmed facebook" height={30} width={30} style={{ marginRight: "10px", width: "auto", height: "30px " }} />
-            <Image src="/instagram.png" alt="icon sosmed facebook" height={30} width={30} style={{ width: "auto", height: "30px " }} />
+      <div className="bg-[#2F2D2D] h-auto rounded-lg mt-[2rem] px-2 lg:px-10" style={{ marginBottom: "2rem" }}>
+        <Image src="/logo.png" alt="logo in footer" width={100} height={100} className="pt-5" />
+        <div className="flex justify-between mt-2 lg:mt-5">
+          <p className="text-white text-xs lg:text-xl lg:w-[40rem] w-[15rem]">Terima kasih telah mengunjungi situs kami. Kami senang bisa berbagi dengan Anda.</p>
+          <div>
+            <p className="text-[#fff] text-xs">Hubungi kami</p>
+            <div className="flex mt-2 justify-end">
+              <Image src="/facebook.png" alt="icon sosmed facebook" height={30} width={30} style={{ marginRight: "10px", width: "auto", height: "20px " }} />
+              <Image src="/youtube.png" alt="icon sosmed facebook" height={30} width={30} style={{ marginRight: "10px", width: "auto", height: "20px " }} />
+              <Image src="/instagram.png" alt="icon sosmed facebook" height={30} width={30} style={{ width: "auto", height: "20px " }} />
+            </div>
           </div>
         </div>
-        <div className="bg-[#D9D9D9] w-[100%] h-1 rounded-lg mt-2"></div>
-        <div className="flex justify-between mt-5">
-          <Image src="/logo.png" alt="logo in footer" width={100} height={100} className="" />
-          <ul className="flex text-[#fff]">
-            <li style={{ marginRight: "1rem" }}>
-              <Link href="/">Beranda</Link>
-            </li>
-            <li style={{ marginRight: "1rem" }}>
-              <Link href="/galeri">Galeri</Link>
-            </li>
-            <li style={{ marginRight: "1rem" }}>
-              <Link href="/pengumuman">Pengumuman</Link>
-            </li>
-            <li style={{ marginRight: "1rem" }}>
-              <Link href="/profil">Profil</Link>
-            </li>
-            <li>
-              <Link href="/jadwal">Jadwal</Link>
-            </li>
-          </ul>
-          <p className="text-[#fff]">© 2024 ARGA PRATAMA</p>
+        <div className="bg-[#D9D9D9] w-[100%] h-[1px] lg:h-1 rounded-lg mt-2"></div>
+        <div className="flex justify-end mt-5">
+          <p className="text-[#fff] text-xs">© 2024 ARGA PRATAMA</p>
         </div>
+        <div className="h-2"></div>
       </div>
       {/* end footer */}
-      <div className="h-[0.5rem] w-[100rem]"></div>
     </main>
   );
 };
