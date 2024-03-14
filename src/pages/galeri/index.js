9d1f8c5fa6fc61f104686c41711070433df50eef
Arga <argaprataman052@gmail.com>
2024-03-14 13:24:37 +0700

    kategori pengumuman,galeri

--- src/pages/galeri/index.js
+++ src/pages/galeri/index.js
@@ -19,7 +19,12 @@ const Galeri = () => {
   const [galeriData, setGaleriData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [showScrollButton, setShowScrollButton] = useState(false);
-  const [selectedCategory, setSelectedCategory] = useState(null); // State untuk menyimpan kategori yang dipilih
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const categories = ["keagamaan", "kesenian", "teknologi", "bela-diri", "organisasi", "pkk", "olahraga", "bahasa"];
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category); // Tangani perubahan kategori yang dipilih
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -47,39 +52,11 @@ const Galeri = () => {
     setSearchTerm(e.target.value);
   };
 
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
-  };
-
-  const filteredData = galeriData.filter((galeri) => {
-    // Jika tidak ada kategori yang dipilih, tampilkan semua data
-    if (selectedCategory === null) {
-      return galeri.name.toLowerCase().includes(searchTerm.toLowerCase());
-    } else {
-      // Jika kategori telah dipilih, tampilkan hanya data dengan kategori yang sesuai
-      return galeri.kategori === selectedCategory && galeri.name.toLowerCase().includes(searchTerm.toLowerCase());
-    }
-  });
-
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-  };
-
-  const downloadImage = (imageUrl) => {
-    fetch(imageUrl)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement("a");
-        link.href = url;
-        link.setAttribute("download", "image.jpg");
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-      });
   };
 
   return (
@@ -97,66 +74,48 @@ const Galeri = () => {
             onChange={handleSearchChange}
           />
         </div>
-        <div className="flex ms-10 flex-wrap mt-5 md:mt-0">
-          {/* Tombol kategori */}
-          <button
-            className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "Keagamaan" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`}
-            onClick={() => handleCategoryChange("Keagamaan")}
-          >
-            <p className="text-xs md:text-lg">Keagamaan</p>
-          </button>
-          <button
-            className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "Teknologi" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`}
-            onClick={() => handleCategoryChange("Teknologi")}
-          >
-            <p className="text-xs md:text-lg">Teknologi</p>
-          </button>
-          <button className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "Kesenian" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`} onClick={() => handleCategoryChange("Kesenian")}>
-            <p className="text-xs md:text-lg">Kesenian</p>
-          </button>
-          <button className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "olahraga" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`} onClick={() => handleCategoryChange("olahraga")}>
-            <p className="text-xs md:text-lg">Olahraga</p>
-          </button>
-          <button className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "PKK" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`} onClick={() => handleCategoryChange("PKK")}>
-            <p className="text-xs md:text-lg">PKK</p>
-          </button>
-          <button
-            className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "organisasi" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`}
-            onClick={() => handleCategoryChange("organisasi")}
-          >
-            <p className="text-xs md:text-lg">Organisasi</p>
-          </button>
-          <button
-            className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "bela diri" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`}
-            onClick={() => handleCategoryChange("bela diri")}
-          >
-            <p className="text-xs md:text-lg">Bela diri</p>
-          </button>
-          <button className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === "bahasa" ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`} onClick={() => handleCategoryChange("bahasa")}>
-            <p className="text-xs md:text-lg">Bahasa</p>
-          </button>
-
-          {/* Tambahkan tombol kategori lainnya sesuai format yang sama */}
+        <p className="ms-12 mt-3 font-semibold text-[#ffffff] text-sm md:text-xl">Kategori</p>
+        <div className="flex ms-10 flex-wrap mt-0 md:mt-0 ps-4 md:ps-0">
+          {categories.map((category) => (
+            <button
+              key={category}
+              className={`md:m-5 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === category ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`}
+              onClick={() => handleCategoryChange(category)}
+            >
+              <p className="text-xs md:text-lg">{category.charAt(0).toUpperCase() + category.slice(1)}</p>
+            </button>
+          ))}
         </div>
         <div className="mx-2 md:mx-10 grid md:grid-cols-4 grid-cols-1 gap-4 mt-5">
-          {filteredData.map((galeri) => (
-            <div key={galeri.id}>
-              <div className="h-[18rem] bg-[#3d3d3d7a] mx-5 overflow-hidden rounded-lg relative">
-                <div className="flex absolute items-center justify-between w-full z-50">
-                  <div className="px-2 py-1 bg-[#fff] justify-center items-center flex m-2 rounded-2xl drop-shadow-md">
-                    <p className="text-[#000] font-semibold text-[10px]">10 Agustus, 2023</p>
+          {galeriData
+            .filter((galeri) => {
+              // Jika tidak ada kategori yang dipilih, tampilkan semua data
+              if (selectedCategory === null) {
+                return true;
+              } else {
+                // Jika kategori telah dipilih, tampilkan hanya data dengan kategori yang sesuai
+                return galeri.category === selectedCategory;
+              }
+            })
+            // .filter((galeri) => galeri.name.toLowerCase().includes(searchTerm.toLowerCase()))
+            .map((galeri) => (
+              <div key={galeri.id}>
+                <div className="h-[18rem] bg-[#3d3d3d7a] mx-5 overflow-hidden rounded-lg relative">
+                  <div className="flex absolute items-center justify-between w-full z-50">
+                    <div className="px-2 py-1 bg-[#fff] justify-center items-center flex m-2 rounded-2xl drop-shadow-md">
+                      <p className="text-[#000] font-semibold text-[10px]">10 Agustus, 2023</p>
+                    </div>
+                    <div className="unduh px-1 py-1 bg-[#fff] justify-center items-center flex m-2 rounded-full drop-shadow-md">
+                      <Image src="/more.png" alt="" width={20} height={20} />
+                    </div>
                   </div>
-                  <div className="unduh px-1 py-1 bg-[#fff] justify-center items-center flex m-2 rounded-full drop-shadow-md" onClick={() => downloadImage(galeri.image)}>
-                    <Image src="/more.png" alt="" width={20} height={20} />
-                  </div>
+                  <p className="absolute bottom-2 text-xl font-semibold text-[#fff] drop-shadow-2xl z-50" style={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }}>
+                    {galeri.name}
+                  </p>
+                  {galeri.image && <img src={galeri.image} alt={galeri.name} height={700} width={700} objectFit="cover" className="rounded-lg" style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }} />}
                 </div>
-                <p className="absolute bottom-2 text-xl font-semibold text-[#fff] drop-shadow-2xl z-50" style={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }}>
-                  {galeri.name}
-                </p>
-                {galeri.image && <img src={galeri.image} alt={galeri.name} height={700} width={700} objectFit="cover" className="rounded-lg" style={{ position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)" }} />}
               </div>
-            </div>
-          ))}
+            ))}
         </div>
         {/* Scroll-to-top button */}
         {showScrollButton && (
--- src/pages/pengumuman/index.js
+++ src/pages/pengumuman/index.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import Navbar from "@/components/navbar/navbarFeature";
 import React, { useState, useEffect } from "react";
 import { db } from "@/firebase/firebaseConfig.js";
@@ -22,6 +21,12 @@ const Pengumuman = () => {
   const [comments, setComments] = useState([]);
   const [username, setUsername] = useState("");
   const [newComment, setNewComment] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const categories = ["keagamaan", "kesenian", "teknologi", "bela-diri", "organisasi", "pkk", "olahraga", "bahasa"];
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category); // Tangani perubahan kategori yang dipilih
+  };
 
   const handleShowDetail = (id) => {
     setSelectedPost(id);
@@ -110,59 +115,79 @@ const Pengumuman = () => {
       </header>
       <main>
         <div className="md:mx-[5rem] mx-2 grid md:grid-cols-5 gap-4 md:mt-[6rem] mt-[2rem]">
-          <div className="p-3 rounded-lg w-full md:h-[32rem] h-[10rem] col-span-2 mt-2">
+          <div className="mx-5 rounded-lg md:h-[32rem] h-[10rem] col-span-2 mt-6">
             <input type="text" placeholder="cari eskul" className="px-2 py-2 w-full rounded-lg focus:outline-none border border-[#fff] bg-transparent focus:text-[#fff] text-[#ffffff]" value={searchTerm} onChange={handleSearchChange} />
-            <p className="mt-3 font-semibold text-[#ffffff] text-xl">Kategori</p>
-            <div className="mx-2 mt-2">{/* Kategori-kategori di sini */}</div>
+            <p className="mt-3 font-semibold text-[#ffffff] text-sm md:text-xl">Kategori</p>
+            <div className="mx-2">
+              {categories.map((category) => (
+                <button
+                  key={category}
+                  className={`md:m-1 m-1 border border-[#fff] p-2 rounded-lg hover:bg-[#fff] hover:text-[#000] ${selectedCategory === category ? "bg-[#fff] text-[#000]" : "text-[#fff]"}`}
+                  onClick={() => handleCategoryChange(category)}
+                >
+                  <p className="text-xs md:text-xs">{category.charAt(0).toUpperCase() + category.slice(1)}</p>
+                </button>
+              ))}
+            </div>
           </div>
-          <div className="col-span-2 md:mx-0 mx-1">
-            {pengumumanData.map((pengumuman) => (
-              <div key={pengumuman.id}>
-                <div className="h-auto bg-[#f0eded] p-5 rounded-xl mt-2">
-                  <div className="h-[12rem] w-full bg-[#00000083] flex justify-center items-center">
-                    {pengumuman.image && <img src={pengumuman.image} alt={pengumuman.name} style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "cover" }} />}
-                  </div>
-                  <p className="mt-2 font-bold text-3xl">{pengumuman.name}</p>
-                  <p className="mt-2">{pengumuman.pengumuman.length > 100 ? pengumuman.pengumuman.substring(0, 100) + "..." : pengumuman.pengumuman}</p>
-                  <div className="flex items-center mt-5">
-                    <div className="h-7 w-[7rem] bg-[#e9e1e1a1] rounded-xl flex items-center justify-center">
-                      <div className="h-5 w-5 bg-white rounded-full items-center flex justify-center">
-                        <Image src="/update.png" alt="" height={10} width={10} />
-                      </div>
-                      <p className="text-[10px] px-1">3, Januari, 2023</p>
+          <div className="col-span-2 md:mx-0 mx-1 mt-3">
+            {pengumumanData
+              .filter((pengumuman) => {
+                // Jika tidak ada kategori yang dipilih, tampilkan semua data
+                if (selectedCategory === null) {
+                  return true;
+                } else {
+                  // Jika kategori telah dipilih, tampilkan hanya data dengan kategori yang sesuai
+                  return pengumuman.kategori === selectedCategory;
+                }
+              })
+              .map((pengumuman) => (
+                <div key={pengumuman.id}>
+                  <div className="h-auto bg-[#f0eded] p-5 rounded-xl mt-2">
+                    <div className="h-[12rem] w-full bg-[#00000083] flex justify-center items-center">
+                      {pengumuman.image && <img src={pengumuman.image} alt={pengumuman.name} style={{ maxWidth: "100%", maxHeight: "100%", objectFit: "cover" }} />}
                     </div>
-                    <div className="w-7 h-7 bg-[#e9e1e1a1] flex items-center justify-center rounded-full ms-3" onClick={() => handleShowDetail(pengumuman.id)}>
-                      <Image src="/speech-bubble.png" alt="" height={15} width={15} />
-                    </div>
-                  </div>
-                </div>
-                {selectedPost === pengumuman.id && (
-                  <div className="h-auto bg-[#f0eded] p-5 rounded-xl mt-1">
-                    <p>{pengumuman.pengumuman}</p>
-                    <p className="mt-5 font-semibold">Komentar {comments.length}</p>
-                    <div className="h-[1px] w-full bg-[#2b2a2a5b] mt-1"></div>
-                    <div className="column-comment">
-                      {comments.map((comment, index) => (
-                        <div key={index} className="mt-3">
-                          <p className="font-semibold">{comment.username}</p>
-                          <p>{comment.comment}</p>
+                    <p className="mt-2 font-bold text-3xl">{pengumuman.name}</p>
+                    <p className="mt-2">{pengumuman.pengumuman.length > 100 ? pengumuman.pengumuman.substring(0, 100) + "..." : pengumuman.pengumuman}</p>
+                    <div className="flex items-center mt-5">
+                      <div className="h-7 w-[7rem] bg-[#e9e1e1a1] rounded-xl flex items-center justify-center">
+                        <div className="h-5 w-5 bg-white rounded-full items-center flex justify-center">
+                          <Image src="/update.png" alt="" height={10} width={10} />
                         </div>
-                      ))}
-                    </div>
-                    <div className="h-[1px] w-full bg-[#2b2a2a5b] mt-1"></div>
-                    <div className="mt-3">
-                      <div className="grid grid-cols-2 gap-2">
-                        <input type="text" placeholder="Masukkan nama" value={username} onChange={(e) => setUsername(e.target.value)} className="py-2 px-4 focus:outline-none rounded-lg mt-2" />
-                        <input type="text" placeholder="Berikan komentar" value={newComment} onChange={(e) => setNewComment(e.target.value)} className="py-2 px-4 focus:outline-none rounded-lg mt-2" />
+                        <p className="text-[10px] px-1">3, Januari, 2023</p>
                       </div>
-                      <button onClick={addComment} className="py-2 px-4 bg-blue-500 text-white rounded-lg mt-2 w-full">
-                        Kirim
-                      </button>
+                      <div className="w-7 h-7 bg-[#e9e1e1a1] flex items-center justify-center rounded-full ms-3" onClick={() => handleShowDetail(pengumuman.id)}>
+                        <Image src="/speech-bubble.png" alt="" height={15} width={15} />
+                      </div>
                     </div>
                   </div>
-                )}
-              </div>
-            ))}
+                  {selectedPost === pengumuman.id && (
+                    <div className="h-auto bg-[#f0eded] p-5 rounded-xl mt-1">
+                      <p>{pengumuman.pengumuman}</p>
+                      <p className="mt-5 font-semibold">Komentar {comments.length}</p>
+                      <div className="h-[1px] w-full bg-[#2b2a2a5b] mt-1"></div>
+                      <div className="column-comment">
+                        {comments.map((comment, index) => (
+                          <div key={index} className="mt-3">
+                            <p className="font-semibold">{comment.username}</p>
+                            <p>{comment.comment}</p>
+                          </div>
+                        ))}
+                      </div>
+                      <div className="h-[1px] w-full bg-[#2b2a2a5b] mt-1"></div>
+                      <div className="mt-3">
+                        <div className="grid grid-cols-2 gap-2">
+                          <input type="text" placeholder="Masukkan nama" value={username} onChange={(e) => setUsername(e.target.value)} className="py-2 px-4 focus:outline-none rounded-lg mt-2" />
+                          <input type="text" placeholder="Berikan komentar" value={newComment} onChange={(e) => setNewComment(e.target.value)} className="py-2 px-4 focus:outline-none rounded-lg mt-2" />
+                        </div>
+                        <button onClick={addComment} className="py-2 px-4 bg-blue-500 text-white rounded-lg mt-2 w-full">
+                          Kirim
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
           </div>
         </div>
         {showScrollButton && (
