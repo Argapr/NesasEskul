147676e84bb7b28b682840c81f35be545c40c372
Arga <argaprataman052@gmail.com>
2024-08-05 04:42:03 +0700

    feat: i

--- src/pages/pengumuman/index.js
+++ src/pages/pengumuman/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Navbar from "../../components/navbar/navbarFeature.jsx";
+import Navbar from "../../components/Navbar/navbarFeature.jsx";
 import React, { useState, useEffect } from "react";
 import { db } from "../../firebase/firebaseConfig";
 import { collection, getDocs, addDoc, query, where, onSnapshot } from "firebase/firestore";
